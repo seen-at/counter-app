8e71a5823fefd92aaf19148d7f2615998547fe4a
seen-at <seenatibrahim@gmail.com>
2022-06-18 19:00:09 +0600

    Basic counter app with previous entry records

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+let saveEl = document.getElementById("save-el");
+console.log(saveEl);
+
+function save() {
+    let countStr = count + " - "
+    console.log(count)
+    saveEl.textContent += countStr
+    countEl.innerText = 0
+    count = 0
+}
